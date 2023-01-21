677ed9549f38de3c796c5fdca1acfbaa2ff3bc0b
Artem Ligerko <artmarket81@gmail.com>
2023-01-21 23:17:11 +0200

    fix: some changes for while testing client side

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ mongoose.set("strictQuery", false);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use("/api", router);
 app.use(errorMiddleware); // must be last in middlewares chain
 
